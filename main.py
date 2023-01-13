6467c43acca2802ec294470a730c81cba5472a7b
Shivam <shivamverma2908@gmail.com>
2023-01-13 22:51:07 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,71 @@
+def get_todo(filepath="todos.txt"):
+    with open(filepath, 'r') as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+
+def write_todo(todos_arg, filepath="todos.txt"):
+    with open(filepath, 'w') as file_local:
+        file_local.writelines(todos_arg)
+
+
+while True:
+    user_input = input("Enter add, show, edit, complete or exit : ")
+    user_input = user_input.strip()
+
+    if user_input.startswith('add'):
+        new_todo = user_input[4:] + "\n"
+
+        todos = get_todo()
+
+        todos.append(new_todo)
+
+        write_todo(todos)
+
+    elif user_input.startswith('show'):
+        todos = get_todo()
+
+        # new_todos = [item.strip('\n') for item in todos]
+
+        for index, item in enumerate(todos):
+            item = item.strip('\n')
+            row = f"{index+1}-{item}"
+            print(row)
+
+    elif user_input.startswith('edit'):
+        try:
+            edit_number = int(user_input[5:])
+            edit_number -= 1
+            new_todo = input("Enter new todo : ") + "\n"
+            todos = get_todo()
+
+            todos[edit_number] = new_todo
+            print("Edited!")
+
+            write_todo(todos)
+        except ValueError:
+            print("Enter the correct index.")
+            continue
+
+    elif user_input.startswith('complete'):
+        try:
+            number = int(user_input[9:])
+            number -= 1
+
+            todos = get_todo()
+
+            deleted_todo = todos[number].strip('\n')
+            todos.pop(number)
+
+            write_todo(todos)
+            message = f"todo {deleted_todo} was removed."
+            print(message)
+        except IndexError:
+            print("Enter a valid index.")
+            continue
+    elif user_input.startswith('exit'):
+        break
+    else:
+        print("Command not valid.")
+
+print("Bye!")
